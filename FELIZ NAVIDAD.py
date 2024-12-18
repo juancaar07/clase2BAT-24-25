f0dd7ec3ac7e9c93e1b8f100c6ff35f96bdd6f98
Juancaar007 <juasangar15@alu.edu.gva.es>
2024-12-18 13:19:21 +0100

    IES18D

---
+++ FELIZ NAVIDAD.py
@@ -0,0 +1,7 @@
+#ÁRBOL DE NAVIDAD DE LOS ALUMNOS DE SEGUNDO DE BACHILLERATO 24/25 INFORMÁTICA 
+
+print(" *** *** *   * ***     **   *   *    *    * * ***   *   ***")
+print(" **  **  *   *  *      *  * *  * *    *  *  * *  * * *  *  *")
+print(" *   *** *** * ***     *   ** *   *     *   * *** *   * ***")
+
+
